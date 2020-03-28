987910dcd36efd544f83cd56ddf105afc1d03413
J+Q <qin@jplusq.com>
2020-03-28 13:37:43 +1300

    layout

--- js/global.js
+++ js/global.js
@@ -197,36 +197,36 @@ $(function() {
 
     
     // Isotope
-    var $grid = $('.grid').isotope({
-      itemSelector: '.element-item',
-      layoutMode: 'fitRows',
-      columnWidth: '.grid-sizer'
-    });
+    // var $grid = $('.grid').isotope({
+    //   itemSelector: '.element-item',
+    //   layoutMode: 'fitRows',
+    //   columnWidth: '.grid-sizer'
+    // });
     
-    // filter functions
-    var filterFns = {
-      // show if name ends with -ium
-      ium: function() {
-        var name = $(this).find('.name').text();
-        return name.match( /ium$/ );
-      }
-    };
+    // // filter functions
+    // var filterFns = {
+    //   // show if name ends with -ium
+    //   ium: function() {
+    //     var name = $(this).find('.name').text();
+    //     return name.match( /ium$/ );
+    //   }
+    // };
     
-    // bind filter button click
-    $('.filters-button-group').on( 'click', 'div', function() {
-      var filterValue = $( this ).attr('data-filter');
-      // use filterFn if matches value
-      filterValue = filterFns[ filterValue ] || filterValue;
-	  $grid.isotope({ filter: filterValue });
-	  $('.grid').isotope({
-		hiddenStyle: {
-		  opacity: 0
-		},
-		visibleStyle: {
-		  opacity: 1
-		}
-	  });
-    });
+    // // bind filter button click
+    // $('.filters-button-group').on( 'click', 'div', function() {
+    //   var filterValue = $( this ).attr('data-filter');
+    //   // use filterFn if matches value
+    //   filterValue = filterFns[ filterValue ] || filterValue;
+	//   $grid.isotope({ filter: filterValue });
+	//   $('.grid').isotope({
+	// 	hiddenStyle: {
+	// 	  opacity: 0
+	// 	},
+	// 	visibleStyle: {
+	// 	  opacity: 1
+	// 	}
+	//   });
+    // });
     
     // change is-checked class on buttons
     $('.button-group').each( function( i, buttonGroup ) {
